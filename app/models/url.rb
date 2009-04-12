9f7562e40497141496c2979d4c05152a59688357
Steve Woodcock <steve.woodcock@gmail.com>
2009-04-12 17:32:52 +0100

    fetch content

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,19 @@
+require 'open3'
 class Url < ActiveRecord::Base
+  def fetch_content
+    cmd = "lynx -display_charset=utf8 -dump -nolist #{self.url}"
+    out = ''
+    err = ''
+    Open3.popen3(cmd) do |stdin, stdout, stderr|
+      stdin.close
+      err = stderr.read
+      out = stdout.read
+    end
+    if err != ''
+      raise err
+    else
+      self.content = out
+      save
+    end
+  end
 end
--- test/unit/url_test.rb
+++ test/unit/url_test.rb
@@ -2,7 +2,10 @@ require 'test_helper'
 
 class UrlTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should fetch url content" do
+    url = urls(:djangobook)
+    url.fetch_content
+    assert_not_nil url.content
+    assert_match /Caching/, url.content
   end
 end
